828781f47d5d1ca6120a8cd8c97dd0466c547dfe
austin-showen <austin.showen@gmail.com>
2022-04-05 21:55:14 -0700

    Pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    let reverseArray = [];
+    for (i = array.length-1; i >= 0; i--) {
+        reverseArray.push(array[i]);
+    }
+    const reverseString = reverseArray.join("");
+    return reverseString;
 };
 
 // Do not edit below this line
