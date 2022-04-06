b8121788209a7465aaecef34171429823aa825c3
austin-showen <austin.showen@gmail.com>
2022-04-05 22:10:18 -0700

    Pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    const args = Array.from(arguments);
+    const inputArray = args[0];
+    let outputArray = [];
+    const argsToRemove = args.slice(1, args.length);
+    for (i = 0; i < inputArray.length; i++) {
+        if (argsToRemove.indexOf(inputArray[i]) === -1) {
+            outputArray.push(inputArray[i]);
+        }
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
