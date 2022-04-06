370af2b2f9dea4faca9ea5c0af17a25b6c2e65f3
austin-showen <austin.showen@gmail.com>
2022-04-05 22:30:53 -0700

    Pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof(start) != "number" 
+            || typeof(end) != "number"
+            || start < 0
+            || end < 0) {
+        return "ERROR";
+    }
 
+    if (start > end) {
+        let placeholder = start;
+        start = end;
+        end = placeholder;
+    }
+
+    let sum = 0;
+    for (i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
